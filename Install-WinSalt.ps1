68ce95104e3f61283bba2d85cfff933bb6151053
John Singer <jpsthecelt@gmail.com>
2021-06-03 09:48:09 -0700

    initial rep. creation/adding WinSalt installer

---
+++ Install-WinSalt.ps1
@@ -0,0 +1,15 @@
+/    [CmdletBinding()]
+    param (
+        $Src
+    )
+    
+if (-not (Test-Path -Path c:\temp)) {
+    mkdir c:\temp
+}
+if (-not (Test-Path -Path "$env:ProgramData/SALT Software/SALT 20/")) {
+    mkdir "$env:ProgramData/SALT Software/SALT 20/"
+}
+# 
+Copy-Item $Src/* c:/temp/
+msiexec /qn /i "c:\temp\WinSALT20.msi”
+Copy-Item c:/temp/SALT_20_LIC.dat "$env:ProgramData/SALT Software/SALT 20/"
